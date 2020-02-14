6387eb3ac278841271b76cbff3f5d8dd7b66107e
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-02-14 14:23:01 -0600

    api routes pseudo

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -12,19 +12,29 @@ let mongoose = require("mongoose");
 //// module.exports     function(app)
 //////////////////////////////////////////////////////////
 
-// add to exercises array:
-////> type            STRING
-////> name            STRING
-////>
 
-// add a workout:
-////> date (?AUTO set with function? ??)
-//>>> exercises
-///////>> type          STRING
-///////>> name          STRING
-///////>> duration      INTERVAL
-///////>> weight        INTERVAL
-///////>> reps          INTERVAL
-///////>> sets          INTERVAL
+// add exercise:
+//
+// exercise.js..... 
+// handleFormSubmit(event) >>> await API.addexercise(workoutData)
+//
+////> let workoutData = {}
+///////>> workoutData.type          STRING ("cardio" or "resistance")
+///////////>>> if "cardio"...
+//////////////////>>>> workoutData.name          STRING
+//////////////////>>>> workoutData.duration      NUMBER
+//////////////////>>>> workoutData.distance      INTERVAL
+//////////////////>>>> workoutData.duration      INTERVAL
+///////////>>> if "resistance"...
+//////////////////>>>> workoutData.name          STRING
+//////////////////>>>> workoutData.duration      NUMBER
+//////////////////>>>> workoutData.weight        INTERVAL
+//////////////////>>>> workoutData.sets          INTERVAL
+//////////////////>>>> workoutData.reps          INTERVAL
+//////////////////>>>> workoutData.duration      INTERVAL
+
+
+
+
 
 
