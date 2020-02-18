f019c111a21a04677b3fccc8ca6c0c82276a7440
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-02-18 10:28:41 -0600

    api routes fixed

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,30 +11,63 @@ let mongoose = require("mongoose");
 //////////////////////////////////////////////////////////
 //// module.exports     function(app)
 //////////////////////////////////////////////////////////
+var db = require("../models");
 
+module.exports = function (app) {
+    // Creating new workout in the database
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body).then(response => {
+            res.json(response);
+        });
+    });
 
-// add exercise:
-//
-// exercise.js..... 
-// handleFormSubmit(event) >>> await API.addexercise(workoutData)
-//
-////> let workoutData = {}
-///////>> workoutData.type          STRING ("cardio" or "resistance")
-///////////>>> if "cardio"...
-//////////////////>>>> workoutData.name          STRING
-//////////////////>>>> workoutData.duration      NUMBER
-//////////////////>>>> workoutData.distance      INTERVAL
-//////////////////>>>> workoutData.duration      INTERVAL
-///////////>>> if "resistance"...
-//////////////////>>>> workoutData.name          STRING
-//////////////////>>>> workoutData.duration      NUMBER
-//////////////////>>>> workoutData.weight        INTERVAL
-//////////////////>>>> workoutData.sets          INTERVAL
-//////////////////>>>> workoutData.reps          INTERVAL
-//////////////////>>>> workoutData.duration      INTERVAL
+    // Getting most recent workout
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
 
+    // Adding new exercise to existing workout
+    app.post("/api/workouts/:_id", (req, res) => {
+        let exercises = {
+            type: req.body.type,
+            name: req.body.name,
+            duration: req.body.duration,
+            weight: req.body.weight,
+            reps: req.body.reps,
+            sets: req.body.sets,
+            distance: req.body.distance
+        }
+        db.Workout.update({
+            _id: req.params._id
+        },
+            {
+                $push: {
+                    exercises
+                }
+            })
+            //   .then(({}) => db.Workout.findOneAndUpdate({_id: mongojs.ObjectId(req.params.id)}, { $set: { exercises } }, { new: true }))
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
 
-
-
-
-
+    // Viewing workout stats
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+};
